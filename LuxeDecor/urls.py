7535440f26a801a6006dffa8e519e52467481caa
蔡相襄 <78093303+hsiang2@users.noreply.github.com>
2023-06-02 17:13:15 +0800

    新增 product list page

--- LuxeDecor/urls.py
+++ LuxeDecor/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web.views import index
+from web.views import index, product
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', index),
+    path('product/', product),
 ]
